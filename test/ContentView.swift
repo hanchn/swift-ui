17b40f268c8aacbe8dd129f45e1368e4fdf1ad7c
袁晶 <yuanjing@yuanjingdeMacBook-Pro.local>
2021-04-01 10:06:13 +0800

    yuanjing add code

---
+++ test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 袁晶 on 2021/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 袁晶 on 2021/4/1.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
